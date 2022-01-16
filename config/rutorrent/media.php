5a66d022a1789515390162c222ad0a0631b7bb30
hwk <loool@lool.om>
2022-01-16 15:32:32 +0200

    screenshot grid format

--- config/rutorrent/media.php
+++ config/rutorrent/media.php
@@ -3,4 +3,7 @@
 // path on domain where a symlink to view.php can be found
 // change only if you use web AUTH
 // example: http://mydomain.com/stream/view.php
-$streampath = '/rt/plugins/filemanager-media/view.php';
+$streampath = '/plugins/filemanager-media/view.php';
+
+
+$allowedViewFormats = 'avi|divx|mpeg|mp[34]|mkv|png|jpe?g'; // regex format
