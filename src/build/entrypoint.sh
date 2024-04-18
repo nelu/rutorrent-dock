d0c95d29a98c5b68b0d7a5d083c18d734d9dc61a
nelu <unzel@unzel.com>
2024-04-18 23:05:29 +0300

    - volumes for persistent data and tmp dir - proper process signals for graceful stop - updated config.php to latest ruTorrent version - static rar binaries in ./src/build/rar/ - python support added - cleanup config

--- src/build/entrypoint.sh
+++ src/build/entrypoint.sh
@@ -3,16 +3,15 @@
 # auto populate volumes with code
 
 sync_empty() {
-    ! [[ -e "/var/www/html${1}" ]] \
-    && ln -s "$APP_HOME${1}" "/var/www/html${1}" \
-    || {
-        ! [[ "$(ls -A /var/www/html${1})" ]] && ! [[ -L "/var/www/html${1}" ]] && {
-                echo "sync_empty: mount is empty /var/www/html${1}"
-                ls -A "/var/www/html${1}"
-                cp -rfp "$APP_HOME${1}" "/var/www/html/"
-        }
-    }
+    ! [[ -e "${1}" ]] && [[ -e "${2}" ]] && { \
+      echo "sync_empty: dir is empty ${1} - copy from ${2}";
+      cp -rfp "${2}" "${1}"
+      }
+
 }
-sync_empty ""
-sync_empty "/share"
-apache2-foreground
+
+! [[ -e "/var/www/html" ]] && ln -s "$APP_HOME" "/var/www/html"
+
+sync_empty "/data/share" "$APP_HOME/share"
+
+exec apache2-foreground "$@"
