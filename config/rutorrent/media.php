abd7c7183691e5ff64f5b62bfd2250edb60c047f
nelu <nelu@github.com>
2025-01-05 07:22:59 +0200

    add 7zip-rar package to image filemanager-media endpoint config in env cleanup

--- config/rutorrent/media.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// path on domain where a symlink to view.php can be found
-// change only if you use web AUTH
-// example: http://mydomain.com/stream/view.php
-$streampath = '/plugins/filemanager-media/view.php';
-
-
-$allowedViewFormats = 'avi|divx|mpeg|mp[34]|mkv|png|jpe?g'; // regex format
